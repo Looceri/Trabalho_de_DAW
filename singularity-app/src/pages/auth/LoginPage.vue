db5cd339680ae8060731372f8398f80dde664d70
Loocer <keltongameplays@gmail.com>
2024-10-14 20:49:58 +0200

    Tela do Login finalizada

--- singularity-app/src/pages/auth/LoginPage.vue
+++ singularity-app/src/pages/auth/LoginPage.vue
@@ -1,63 +1,101 @@
 <template>
-  <q-page class="flex bg-white">
-    <div class="q-pa-md q-mx-auto flex flex-center flex-column w-80 q-gutter-md">
+  <q-page class="q-pa-md flex flex-center" style="">
+    <div class="q-mx-auto q-pa-md q-gutter-md" style="max-width: 400px; width: 100%">
 
-      <div>
-        <!-- Boas-vindas -->
-        <div class="text-h4 text-bold q-mb-sm text-center">Bem-vindo de volta</div>
-        <div class="text-subtitle1 text-center q-mb-md">
+      <!-- Título de boas-vindas -->
+      <div style="margin-bottom: 100px;">
+        <div class="text-h5 title-color text-bold text-center bg-as q-mb-sm">
+          Bem-vindo de volta
+        </div>
+        <div class="text-subtitle2 text-center q-mb-md">
           Explore novas oportunidades de emprego e acompanhe suas candidaturas.
         </div>
       </div>
 
       <!-- Campo de Email -->
-      <div class="flex">
-        <div>
-          <h7>Email</h7>
-          <div class="q-mb-sm full-width">
-            <q-input class="col-9" filled v-model="email" label="Email" hint="Digite seu email" lazy-rules
-              :rules="[val => val && val.length > 0 || 'Por favor, insira um email válido']" />
-          </div>
+      <div>
+        <LabelInput label="Email" />
+        <q-input borderless v-model="email" :dense="true" placeholder="Digite seu email" type="email" class="q-px-md"
+          style="background-color:white ;" />
+      </div>
 
+      <!-- Campo de Senha com Alternância de Visibilidade -->
+      <div>
+        <LabelInput label="Senha" />
+        <q-input borderless v-model="password" :dense="true" placeholder="Digite sua senha" :type="passwordFieldType"
+          class="q-px-md">
+          <template v-slot:append>
+            <q-icon :name="showPassword ? 'visibility' : 'visibility_off'" @click="togglePasswordVisibility"
+              class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+
+      <!-- Lembre-se e Esqueceu a Senha (mesma linha) -->
+      <div class="row items-center q-mb-md justify-between full-width q-gutter-sm" style="margin: 0;">
+        <div class="col-6">
+          <q-checkbox v-model="remember" class="text-caption" label="Lembre-se de mim" color="primary" />
         </div>
-
-
-        <!-- Campo de Senha -->
-        <div>
-          <h7>Senha</h7>
-          <div class="q-mb-md">
-            <q-input filled v-model="password" type="password" label="Senha" hint="Digite sua senha" lazy-rules
-              :rules="[val => val && val.length > 0 || 'A senha é obrigatória']" />
-          </div>
+        <div class="col-4 text-right">
+          <a class="text-caption text-primary">Esqueceu senha?</a>
         </div>
+      </div>
+
+
+
+      <div>
+
+        <!-- Botão de Login -->
+        <q-btn label="LOGIN" color="primary" class="q-mb-md full-width q-py-md" />
+
+        <!-- Botão de Login com Google -->
+        <q-btn color="accent" class="q-mb-md full-width q-py-md">
+          <img src="../../assets/icons/google.svg" alt="Google Logo"
+            style="width: 24px; height: 24px; margin-right: 8px;" />
+          SIGN IN COM GOOGLE
+        </q-btn>
 
       </div>
 
-      <!-- Botões -->
-      <div class="q-mb-sm flex justify-between">
-        <q-btn color="primary" label="Entrar" class="q-mr-sm" />
-        <q-btn flat color="secondary" label="Esqueceu a senha?" />
+      <!-- Link para Criar Conta (mesma linha) -->
+      <div class="row items-center q-mb-md justify-around full-width q-gutter-sm" style="margin: 0;">
+        <div class="col-6">
+          <div class="text-caption">Ainda não tem uma conta?</div>
+        </div>
+        <div class="col-4 text-right">
+          <a class="text-caption text-primary">Crie uma agora</a>
+        </div>
       </div>
 
-      <!-- Link para Criar Conta -->
-      <div class="text-caption text-center">
-        Ainda não tem uma conta?
-        <a href="" class="text-primary">Crie uma agora</a>
-      </div>
 
     </div>
-
   </q-page>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, computed } from "vue";
+import LabelInput from '../../components/LabelInput.vue';
 
-const email = ref(null);
-const password = ref(null);
+const email = ref("");
+const password = ref("");
+const remember = ref(false);
 
+// Estado para alternar a visibilidade da senha
+const showPassword = ref(false);
+
+// Computed para alternar o tipo do campo de senha entre 'password' e 'text'
+const passwordFieldType = computed(() =>
+  showPassword.value ? "text" : "password"
+);
+
+// Função para alternar a visibilidade da senha
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
 </script>
 
 <style scoped>
-/* Animation for the spinning logo */
+.no-border {
+  border: none !important;
+}
 </style>
