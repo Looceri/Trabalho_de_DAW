2cf5b3c58c9a57269dae4f341355402064147a18
Edilton Magudo <142337151+EdMagudo@users.noreply.github.com>
2024-11-06 13:40:40 +0200

    redirect nas rotas

--- singularity-app/src/router/routes.js
+++ singularity-app/src/router/routes.js
@@ -3,32 +3,29 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-
-    { path: '', name:'index', component: () => import('pages/auth/IndexPage.vue') },
-    { path: 'verify', name:'verify', component: () => import('src/pages/auth/verifyMail.vue') },
-    { path: 'login', name:'login', component: () => import('pages/auth/LoginPage.vue') },
-    { path: 'account', name:'account', component: () => import('pages/auth/AccountPage.vue') },
-    { path: 'open_email', name:'open-email', component: () => import('pages/auth/OpenEmailPage.vue') },
-    { path: 'sucess', name:'sucess', component: () => import('pages/auth/SucessPage.vue') },
-    { path: 'portal', name:'portal', component: () => import('pages/auth/PortalPage.vue') },
+      { path: '', name: 'index', component: () => import('pages/auth/IndexPage.vue') },
+      { path: 'verify', name: 'verify', component: () => import('src/pages/auth/verifyMail.vue') },
+      { path: 'login', name: 'login', component: () => import('pages/auth/LoginPage.vue') },
+      { path: 'account', name: 'account', component: () => import('pages/auth/AccountPage.vue') },
+      { path: 'open_email', name: 'open-email', component: () => import('pages/auth/OpenEmailPage.vue') },
+      { path: 'sucess', name: 'sucess', component: () => import('pages/auth/SucessPage.vue') },
+      { path: 'portal', name: 'portal', component: () => import('pages/auth/PortalPage.vue') },
     ]
   },
   {
-  path: '/home',
-  component: () => import('layouts/AuthLayout.vue'),
-  children: [
-  { path: 'description', name:'description', component: () => import('pages/home/DescriptionPage.vue') },
-  { path: 'upload', name:'upload', component: () => import('pages/home/uploadPage.vue') },
-  { path: 'connection', name:'connection', component: () => import('pages/home/connectionPage.vue') },
-  ]
-},
-  // Always leave this as last one,
-  // but you can also remove it
+    path: '/home',
+    component: () => import('layouts/AuthLayout.vue'),
+    redirect: { name: 'description' }, // Redireciona para a página de descrição de vagas automaticamente
+    children: [
+      { path: 'description', name: 'description', component: () => import('pages/DescriptionPage.vue') },
+      { path: 'account', name: 'home-account', component: () => import('pages/auth/AccountPage.vue') },
+    ]
+  },
   {
     path: '/:catchAll(.*)*',
     name: 'errorNotFound',
     component: () => import('pages/ErrorNotFound.vue')
   }
-]
+];
 
-export default routes
+export default routes;
